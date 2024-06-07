de1bc8ff33b465d3ac73b5c9614e2191f55dd5b6
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-07 22:18:56 +0000

    added some examples to help test the code, and cleaned a little

--- Scientific Computing with Python (Beta)/Time_Calculator/time_cal.py
+++ Scientific Computing with Python (Beta)/Time_Calculator/time_cal.py
@@ -12,7 +12,7 @@ def add_time(start, duration):
         start_hour = int(start[0])
         start_minutes = int(start[2] + start[3])
         
-    print(start_hour, start_minutes) # print to make sure
+    # print(start_hour, start_minutes) # print to make sure
 
     duration_hour = 0
     duration_minutes = 0
@@ -23,13 +23,13 @@ def add_time(start, duration):
         duration_hour = int(duration[0])
         duration_minutes = int(duration[2] + duration[3])
         
-    print(duration_hour, duration_minutes) # print to make sure
+    # print(duration_hour, duration_minutes) # print to make sure
 
     
     end_hour = (start_hour + duration_hour + ((start_minutes + duration_minutes) // 60))%12
-    print(end_hour)
+    # print(end_hour)
     end_minutes = (start_minutes + duration_minutes)%60    
-    print(end_minutes)
+    # print(end_minutes)
 
     AM = (AM + (start_hour + duration_hour + ((start_minutes + duration_minutes) // 60))//12)%2
 
@@ -41,9 +41,17 @@ def add_time(start, duration):
     
     if end_minutes < 10:
         return str(end_hour) + ':0' + str(end_minutes) + ' ' + sufx
-    new_time = str(end_hour) + ':' + str(end_minutes) + ' ' + sufx
     
 
+    return str(end_hour) + ':' + str(end_minutes) + ' ' + sufx
 
 
-    return new_time
+
+
+
+
+''' test examples to run code '''
+
+# print(add_time('3:30 PM', '2:12'))
+# print(add_time('11:55 AM', '3:12'))
+print(add_time('8:16 PM', '69:02'))
